33315b5c0549382c0936f41b53599da6c76f0c06
Quangwx90 <92023476+Quangwx90@users.noreply.github.com>
2021-10-08 15:08:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "dành riêng cho người con gái tôi yêu ",
+    introTitle: 'bebugaming',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,7 +8,7 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'tán được em là bài toán khó ',
     desc: 'anh sẽ dùng thuật toán lập trình để giải bài toán khó đi vào tim em  =)))  ',
-    btnYes: 'yêu',
+    btnYes: 'yêu ',
     btnNo: 'Không bao giờ  nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100037008392536' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100033388093892' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
